6708c597a0608812b0d7d757782e04e68551f7bc
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-07-08 18:45:33 +0800

    Added Lecture of Functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,7 +8,30 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
 
+//Functions
+function logger() {
+    console.log('My name is Jonas');
+}
 
+// Calling / Running / Invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
 
 
