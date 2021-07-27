40f245078e8e8a6009aa0a6d2c50ab7296724ee5
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-07-27 18:44:56 +0800

    Added Lecture of Function Declarations vs Expressions

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
-*/
+
 
 //Functions
 function logger() {
@@ -33,5 +33,26 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+//Function Declarations vs Expressions
+
+//Function Declaration
+// 1991 = Parameter
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+
+//Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
 
 
