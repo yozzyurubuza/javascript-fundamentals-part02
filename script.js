ac227a775b1ef513a7e875e116c9695501180415
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-11-08 22:03:03 +0800

    Added lecture about Arrow Functions

--- script.js
+++ script.js
@@ -40,19 +40,38 @@ const num = Number('23');
 //Function Declaration
 // 1991 = Parameter
 
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+
+
+// //Function Expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+//Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear; // Good for one-liner functions
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
 }
-const age1 = calcAge1(1991);
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
 
 
-//Function Expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
-const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+
 
 
 
