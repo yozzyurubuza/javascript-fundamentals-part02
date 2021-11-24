162c2d52a6bc805581aa771f844bbf4ed99bc800
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-11-25 01:03:03 +0800

    Added lecture of Dot vs Bracket Notation

--- script.js
+++ script.js
@@ -195,16 +195,26 @@ const num = Number('23');
 
 // Introduction to Objects
 
-const jonasArray = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    teacher,
-    ['Michael', 'Peter', 'Steven']
-];
+// const jonasArray = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     teacher,
+//     ['Michael', 'Peter', 'Steven']
+// ];
 
-// Property : Value
-//Object Literal Syntax - order of values does not matter
+// // Property : Value
+// //Object Literal Syntax - order of values does not matter
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// }
+
+//Dot vs Bracket Notation
+
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
@@ -213,7 +223,58 @@ const jonas = {
     friends: ['Michael', 'Peter', 'Steven']
 }
 
-//
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+//Use bracket notation when you need to compute to get the property
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey);
+
+//Sample use case of bracket notation
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+//Insert property and value to object
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtmann';
+
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael."
+
+const firstName = jonas.firstName;
+const numOfFriends = jonas.friends;
+const bestFriend = numOfFriends.shift();
+
+console.log(`${firstName} has ${numOfFriends.length} friends, and his best friend is called ${bestFriend}.`);
+
+
+//Jonas Schmedtmann Implementation
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+
+//Objec Methods
+
+
+
+
+
+
+
+
+
+
 
 
 
