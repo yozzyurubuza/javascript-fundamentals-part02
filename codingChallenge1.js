e04dbf542d4f874f2a9370d1a8e8c392ac3fc385
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-12-02 02:46:19 +0800

    Added Retry of Coding Challenge #1

--- codingChallenge1.js
+++ codingChallenge1.js
@@ -80,8 +80,8 @@ checkWinner(scoreDolphins, scoreKoalas);
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const scoreDolphins = calcAverage(85, 54, 41);
-const scoreKoalas = calcAverage(23, 34, 27);
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
     let winnerMessage;
