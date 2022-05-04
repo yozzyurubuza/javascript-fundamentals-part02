e4acfd2cc426af573c040cd9668658b066c1d16e
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-05-05 01:23:58 +0800

    Revise Reviewing Functions lecture

---
+++ lectures/05-ReviewFunc.js
@@ -0,0 +1,26 @@
+//Reviewing Functions
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear); //2037 - birthYear
+
+  //Compute after getting the data above
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+//Jonas retires in 19 years.
+
+console.log(yearsUntilRetirement(1950, "Mike"));
+//Mike has already retired
--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 "use strict";
 
-//Reviewing Functions
-
-// const calcAge = function (birthYear) {
-//     return 2037 - birthYear;
-// }
-
-// const yearsUntilRetirement = function (birthYear, firstName) {
-//     const age = calcAge(birthYear);
-//     const retirement = 65 - age;
-
-//     if (retirement > 0) {
-//         console.log(`${firstName} retires in ${retirement} years`);
-//         return retirement;
-//     } else {
-//         console.log(`${firstName} has already retired`);
-//         return -1;
-//     }
-// }
-
-// console.log(yearsUntilRetirement(1991, 'Jonas'));
-// console.log(yearsUntilRetirement(1950, 'Mike'));
-
 //Introduction to Arrays
 
 // const friend1 = 'Michael';
