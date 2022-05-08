75cb8fc4fcc273f91523b32d801dd7ce4384d572
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-05-08 12:19:56 +0800

    Revise Intro to Objects lecture

---
+++ lectures/08-IntroToObj.js
@@ -0,0 +1,22 @@
+// Introduction to Objects
+
+//Sample Array
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  teacher,
+  ["Michael", "Peter", "Steven"],
+];
+
+//When converting the array syntax into an object....
+
+// Property : Value / Key : Value
+//Object Literal Syntax - order of the data does not matter
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 "use strict";
 
-// Introduction to Objects
 
-// const jonasArray = [
-//     'Jonas',
-//     'Schmedtmann',
-//     2037 - 1991,
-//     teacher,
-//     ['Michael', 'Peter', 'Steven']
-// ];
-
-// // Property : Value
-// //Object Literal Syntax - order of values does not matter
-// const jonas = {
-//     firstName: 'Jonas',
-//     lastName: 'Schmedtmann',
-//     age: 2037 - 1991,
-//     job: 'teacher',
-//     friends: ['Michael', 'Peter', 'Steven']
-// }
 
 //Dot vs Bracket Notation
 
