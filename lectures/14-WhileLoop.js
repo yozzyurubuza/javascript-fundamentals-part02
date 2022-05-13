4a8871a700b3914b12ed41649cfedafa92992cf0
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-05-14 00:28:43 +0800

    Revise While Loop lecture

---
+++ lectures/14-WhileLoop.js
@@ -0,0 +1,23 @@
+// The While Loop
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+//While Loop version of the for loop above
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+// Loop without counter
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log("Loop is about to end....");
+  }
+}
--- script.js
+++ script.js
@@ -1,32 +1,5 @@
 "use strict";
 
-// The While Loop
-
-// for (let rep = 1; rep <= 10; rep++) {
-//     console.log(`Lifting weights repetition ${rep}`);
-// }
-
-/*
-
-let rep = 1
-while (rep <= 10) {
-    // console.log(`WHILE: Lifting weights repetition ${rep}`);
-    rep++;
-}
-
-let dice = Math.trunc(Math.random() * 6) + 1;
-
-// Loop without counter
-while (dice !== 6) {
-    console.log(`You rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) {
-        console.log('Loop is about to end....');
-    }
-}
-
-*/
-
 // Reviewing Functions
 
 const calcAge = (currentYear, birthYear) => currentYear - birthYear;
